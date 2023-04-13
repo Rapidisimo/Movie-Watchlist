9b5da5adeb36e34282f7b561e217828fbfb4fd5a
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-04-13 19:18:34 -0400

    working and filtered search

--- js/index.js
+++ js/index.js
@@ -1,38 +1,63 @@
 
 const searchField = document.querySelector('.search-bar input')
 const searchButton = document.querySelector('form')
-let movieSearchTitles = []
-let movieSearchDetails = []
 
 
 
-function omdbSearch(searchText) {
+function omdbSearch(searchText) { //Use the API to perform a general search
     fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=90353081&s=${searchText}`)
     .then( response => response.json() )
     .then( data => {
-        console.log(data)
         omdbTitleSearch(data)
     })
 }
 
-function omdbTitleSearch(searchData) {
+function omdbTitleSearch(searchData) { //Do another search but with titles to get more data
     let searchResults
+    let movieSearchTitles = []
     searchResults = searchData.Search
-    searchResults.forEach( function(movies) {
+    searchResults.forEach( function(movies) { //Make an array with movie titles
         movieSearchTitles.push(movies.Title)
     })
     
-    for(let i = 0; i < movieSearchTitles.length; i++) {
-        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=90353081&t=${movieSearchTitles[i]}`)
+    let groupOfMovies = []
+    movieSearchTitles.forEach( movieData => { //Search using the array of movie titles
+        fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=90353081&t=${movieData}`)
         .then( response => response.json() )
         .then( data => {
-            movieSearchDetails.push(data)
+            groupOfMovies.push(data) //Creat an array of the results
+            buildResults(data)
         })
+    })
+
+    function buildResults(data) { //Make HTML but exclude bad results
+        const movieResults = document.querySelector('.results')
+        if(data.Response !== 'False') {
+            movieResults.innerHTML += `
+            <section class="movie">
+                <div class="poster">
+                    <img src="${data.Poster}" alt="movie-poster">
+                </div>
+                <div class="movie-info">
+                    <h1>${data.Title}</h1>
+                    <div class="movie-details">
+                    <p>${data.Runtime}</p>
+                    <p>${data.Genre}</p>
+                    <button>+ Watchlist</button>
+                    </div>
+                    <div class="plot">
+                        <p>
+                        ${data.Plot}
+                        </p>
+                    </div>    
+                </div>
+            </section>
+        `
+        }
     }
-    console.log(movieSearchDetails)
 }
 
-searchButton.addEventListener('submit', (e) => {
+searchButton.addEventListener('submit', (e) => { //Listen for search queries
     e.preventDefault()
     movieSearchTitles = []
     movieSearchDetails = []
