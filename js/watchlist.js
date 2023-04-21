be31c9f506562d4c0791af7a962b65cbeba80b7b
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-04-21 11:39:04 -0400

    css readmore and initial localstorage work

--- js/watchlist.js
+++ js/watchlist.js
@@ -53,80 +53,26 @@ function omdbTitleSearch(searchData) { //Step 3 - Do another search but with tit
 
 
     function buildResults(data) { // Step 5 - Make HTML from search with titles but exclude bad results
-        console.log(data)
-        Object.assign(this, data)
-        const {Ratings, Poster, Title, Runtime, Genre, Plot} = this;
-        const imdbRating = Ratings.length > 0 ? Ratings[0].Value.slice(0,3) : 'N/A'; //Filter out errors when there's no ratings
         if(data.Response !== 'False') {
             movieResults.innerHTML += `
                 <section class="movie">
                     <div class="poster">
-                        <img src="${Poster}" alt="movie-poster">
+                        <img src="${data.Poster}" alt="movie-poster">
                     </div>
                     <div class="movie-info">
-                        <h2>${Title} <span class="rating-score">⭐️${imdbRating}</span></h2>
+                        <h2>${data.Title}</h2>
                         <div class="movie-details">
-                            <p>${Runtime}</p>
-                            <p>${Genre}</p>
-                            <button class="watch-list-btn" data-title="${Title}">Watchlist</button>
+                            <p>${data.Runtime}</p>
+                            <p>${data.Genre}</p>
+                            <button>+ Watchlist</button>
                         </div>
                         <div class="plot">
-                            <p class="expand-text">${Plot}</p>
-                            <button class="read-more">Read More</button>
+                            <p>${data.Plot}</p>
                         </div>    
                     </div>
                 </section>
-            `;
-            const expandText = document.querySelectorAll('.read-more')//Expand or constrain plot text
-            expandText.forEach( (plot) => {
-                plot.addEventListener('click', (e) => {
-                    let clicked = e.target.previousElementSibling;
-                    if(clicked.classList.contains('expand-text')) {
-                        clicked.classList.toggle('expand-text')
-                        e.target.innerText = "Read Less"
-                    }else if(!clicked.classList.contains('expand-text')) {
-                        clicked.classList.toggle('expand-text')
-                        e.target.innerText = "Read More"
-                    }
-                })
-            })
+            `
         }
-
-        const addToWatchList = document.querySelectorAll('.watch-list-btn');
-        addToWatchList.forEach( (wlBtn) => {
-            wlBtn.addEventListener('click', (e) => {
-                let addMovie = e.target.dataset.title;
-                if(!localStorage.getItem('Watchlist')) {
-                    localStorage.setItem('Watchlist', JSON.stringify(addMovie))
-                }else if (localStorage.getItem('Watchlist')) {
-                    let currentStorage = localStorage.getItem('Watchlist');
-                    let retrievedStorage = JSON.parse(currentStorage);
-                    let myMovies = [];
-                    myMovies.push(retrievedStorage, addMovie)
-                    localStorage.setItem('Watchlist', JSON.stringify(myMovies))
-                }
-                // const currenWatchlist = localStorage.getItem("movies") ? null : localStorage.setItem("movies");
-                // let currentStorage = JSON.parse(currenWatchlist);
-                // if(!currentStorage === addMovie) {
-                //     let movieArray = [];
-                //     movieArray.push(currentStorage, addMovieString)
-                // } else {console.log(HMMMMMM)}
-
-                // // localStorage.setItem("Watchlist", movie)
-                // // console.log(localStorage.getItem("Watchlist"))
-                
-                // // var testObject = { 'one': 1, 'two': 2, 'three': 3 };
-
-                // // Put the object into storage
-                // localStorage.setItem('testObject', JSON.stringify(testObject));
-
-                // // Retrieve the object from storage
-                // var retrievedObject = localStorage.getItem('testObject');
-
-                // console.log('retrievedObject: ', JSON.parse(retrievedObject));
-            })
-        })
-
         if(moreResults.classList.contains('hidden')) { //enable More Results Btn when search is performed
             moreResults.classList.toggle('hidden')
         }
@@ -154,4 +100,3 @@ searchButton.addEventListener('submit', (e) => {
     searchField.value = null //Clear search field
     movieResults.innerHTML = '' //Clear HTML from Previous Results
 })
-
